bc605418fbea4aa6c1272362d9b4dcd37c577311
ratitu <rogergodoy@gmail.com>
2025-06-07 15:38:15 -0300

    Update init.py

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ import ee
 import streamlit as st
 from google.oauth2 import service_account
 def initialize_ee():
-    service_account_info = st.secrets["gee_service_account"]
+    service_account_info = st.secrets["streamlit-462218"]
     credentials = service_account.Credentials.from_service_account_info(service_account_info)
     ee.Initialize(credentials)
 initialize_ee()
