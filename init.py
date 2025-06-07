dd46cf34eecf1b2527b27aea9c9caf98b65cb521
ratitu <rogergodoy@gmail.com>
2025-06-07 15:33:24 -0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,8 @@
+import ee
+import streamlit as st
+from google.oauth2 import service_account
+def initialize_ee():
+    service_account_info = st.secrets["gee_service_account"]
+    credentials = service_account.Credentials.from_service_account_info(service_account_info)
+    ee.Initialize(credentials)
+initialize_ee()
